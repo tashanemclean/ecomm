1cad671d334e567f7463cdf7b72e1add35e148ac
Tashane Mclean <tashane.mclean@gmail.com>
2019-06-29 16:35:59 -0400

    created a page title component and styled it and inported a few fonts

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,7 +1,8 @@
 import React, {Component} from "react"
 
 import {reduxForm, Field } from "redux-form"
-import SignInForm from "./signinForm";
+import SignInForm from "./signinForm"
+import PageTitle from "../pageTitle"
 
 class SignIn extends Component {
 
@@ -12,6 +13,7 @@ class SignIn extends Component {
     render () {
         return (
             <div className="sign-in">
+                <PageTitle className="sig-in__page-title" title="Login" />
                 <SignInForm onSubmit={this.onSubmit} className='sign-in__form' />
             </div>
         )
---
+++ src/components/pageTitle.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class PageTitle extends Component {
+    render(){
+        const {className, title} = this.props
+        return(
+            <div className={`${className} page-title`}>{title}</div>
+        )
+    }
+}
+
