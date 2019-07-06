fb4bac6de08eccdeb8525ea53cbf9ed1b307d7b3
Tashane Mclean <tashane.mclean@gmail.com>
2019-07-06 11:32:16 -0400

    built base structure for account component

---
+++ src/components/account/accountInformation.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react"
+
+class AccountInformation extends Component {
+    render () {
+        <div>
+            account info
+        </div>
+    }
+}
+
+export default AccountInformation
---
+++ src/components/account/purchaseHistory.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react"
+
+class PurchaseHistory extends Component {
+    render () {
+        <div>
+            purchase history
+        </div>
+    }
+}
+
+export default PurchaseHistory
