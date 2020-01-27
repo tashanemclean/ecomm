5c93de094b033ffc52324f4657f63637acf23380
Tashane Mclean <tashane.mclean@gmail.com>
2020-01-26 20:14:42 -0500

    finished account infromation form

--- src/components/account/accountInformationForm.js
+++ src/components/account/accountInformationForm.js
@@ -62,7 +62,7 @@ class AccountInformationForm extends Component {
                 name="zipcode"
                 component={FormInput}
                 />
-
+                <div className="account-information-form__line"></div>
             {
                 this.state.showPasswords ? 
                     [
@@ -83,7 +83,20 @@ class AccountInformationForm extends Component {
                         title="Confirm Password"
                         placeholder="Confirm Password"
                         name="confirm"
-                        component={FormInput}/> 
+                        component={FormInput}/>,
+                        <Field key={Math.random()} className='account-information-form__update-information'
+                        onClick={()=> this.setState({ showPasswords: false })}
+                        type="submit"
+                        title="Update Information"
+                        name="update-information"
+                        component={FormButton}/>,
+                        <Field key={Math.random()} className='account-information-form__cancel'
+                        onClick={()=> this.setState({ showPasswords: false })}
+                        type="button"
+                        title="Cancel"
+                        name="cancel"
+                        short={true}
+                        component={FormButton}/>
                     ]
                 :
                 <Field className='account-information-form__change-password'
