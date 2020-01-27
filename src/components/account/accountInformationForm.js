095e85324a3c7fa2523652edeee4b484abb253d0
Tashane Mclean <tashane.mclean@gmail.com>
2020-01-26 19:22:56 -0500

    builde long gray button component

--- src/components/account/accountInformationForm.js
+++ src/components/account/accountInformationForm.js
@@ -2,7 +2,7 @@ import React, {Component} from "react"
 
 import {reduxForm, Field } from "redux-form"
 
-import {FormInput, FormButton} from '../formFields'
+import {FormInput, FormButton, LongGrayButton} from '../formFields'
 
 import history from '../../history'
 
@@ -56,20 +56,13 @@ class AccountInformationForm extends Component {
                 />
 
 
-                {/* <Field className='sign-in-form__password'
-                type="password"
-                title="Password"
-                placeholder="Password"
-                name="password"
-                component={FormInput}
-                />
-
-                <Field className='account-information-form__login'
-                onClick={()=> history.push('/account')}
-                type="submit"
-                title="Login"
-                name="login"
-                component={FormButton}/> */}
+                <Field className='account-information-form__change-password'
+                onClick={()=> console.log('tryn show passwords')}
+                type="button"
+                labelTitle="Password"
+                title="Change Password"
+                name="change-password"
+                component={LongGrayButton}/>
 
             </form>
         )
