ba2252c6875fb2cc463fb57c1e0d4e4c9c902a73
Tashane Mclean <tashane.mclean@gmail.com>
2020-02-01 17:38:09 -0500

    strucured cart props products component

--- src/components/shop/shopCart.js
+++ src/components/shop/shopCart.js
@@ -8,10 +8,19 @@ function CartButton({ className, icon}) {
     )
 }
 
-function CartContent({className}) {
+function CartContent({className, products}) {
+    let count = products.length;
     return (
         <div className={`${className} cart-conent`} >
+            <div className='cart-content__title'>
+                Cart {count}
+            </div>
+            <div className='cart-content__products'>
 
+            </div>
+            <div className='cart-content__footer'>
+
+            </div>
         </div>
     )
 }
@@ -22,7 +31,7 @@ class ShopCart extends Component {
         return (
             <div className={`${className} shop-cart`}>
                 <CartButton className='shop-cart__toggle' icon='fas fa-times' />
-                <CartContent className='shop-cart__conent' />
+                <CartContent className='shop-cart__conent' products={[343,4534,545]} />
             </div>
         )
     }
