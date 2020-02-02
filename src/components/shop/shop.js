9270a7be6cab191701846d0b4f4a27c8bbfe1a96
Tashane Mclean <tashane.mclean@gmail.com>
2020-02-01 21:56:28 -0500

    finished styling cart

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -41,16 +41,8 @@ class Shop extends Component {
         this.props.filterProductsWithQuery(fields)
     }
 
-    handleAddToCart = () => {
-        if(document.getElementById('shop-cart').classList.contains('cart-hidden')) {
-            document.getElementById('shop-cart').classList.remove('cart-hidden');
-        } else {
-            document.getElementById('shop-cart').classList.add('cart-hidden');
-        }
-    }
-
     render() {
-        return <ShopCart className='shop__cart' />
+        // return <ShopCart className='shop__cart' />
 
         return (
             <div className='shop'>
@@ -67,7 +59,7 @@ class Shop extends Component {
                 {
                     this.state.showCart ? <ShopCart className='shop__cart' />: ''
                 }
-                <CartButton onClick={this.handleAddToCart} className='shop__cart-button' icon='fas fa-cart-plus' />
+                <CartButton className='shop__cart-button' icon='fas fa-cart-plus' />
             </div>
         )
     }
