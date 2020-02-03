1a2b494a5a4ac7ec1ec068849f5d2fd396fc4f77
Tashane Mclean <tashane.mclean@gmail.com>
2020-02-02 19:28:51 -0500

    finished shipping form component and sub components

--- src/components/information/shippingForm.js
+++ src/components/information/shippingForm.js
@@ -63,7 +63,7 @@ class ShippingForm extends Component {
                 name="back"
                 short={true}
                 component={FormButton}/>
-                <OrderSummary className='shipping-form__summary'/>
+                <OrderSummary className='shipping-form__order-summary'/>
             </form>
         )
     }
